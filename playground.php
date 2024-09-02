c6f59d70af4842f068f5177458ae479a3617b9df
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-09-02 13:24:06 +0200

    testing webhook

--- playground.php
+++ playground.php
@@ -50,7 +50,7 @@
     <iframe width="560" height="315" src="https://www.youtube.com/embed/mw0bU8CpWcg?si=B2Ik0fTazbJ-hgSX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     <br><br>
 
-    <p><strong>One of my favourite cities TESTING WEBHOOK 2</strong></p>
+    <p><strong>One of my favourite cities TESTING WEBHOOK 3</strong></p>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d521079.2330303196!2d17.322576368412324!3d59.32517755320295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f763119640bcb%3A0xa80d27d3679d7766!2sStockholm!5e0!3m2!1sen!2sse!4v1724570301026!5m2!1sen!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </body>
 
